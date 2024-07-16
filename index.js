ece6f056f7517b991f8fbb2189df19d4a4118e29
Rahul <rahulphulbhati@gmail.com>
2024-07-16 11:52:53 +0530

    list container and port mapping

--- index.js
+++ index.js
@@ -9,27 +9,72 @@ const docker = axios.create({
     baseURL: 'http://localhost'
 });
 
+const ContainerTOMaPortMap = {
 
+};
+
+const PortToContainerMap = {
+    
+}
 
 app.use(express.json());
 // Endpoint to start a container
 app.post('/start-container', async (req, res) => {
     try {
+
+        const availablePort = (()=>{
+            for(let i = 8000; i < 9000; i++){
+                if(PortToContainerMap[i] === undefined){
+                    return i;
+                }
+            }
+        })();
+
+        console.log(availablePort);
+        if(!availablePort){
+            return res.json({error : "No available Port"});
+        }
+
         const containerName = `user-container-${Date.now()}`;
+
+        
         // Create container
         const createResponse = await docker.post('/containers/create', {
             Image: 'alpine',  // Use a different Docker image for testing
             Tty: true,
-            Cmd: ['sh', '-c', 'while true; do sleep 1000; done'] // Keeps the container running
+            Cmd: ['sh', '-c', 'while true; do sleep 1000; done'], // Keeps the container running
+            HostConfig:{
+                PortBindings : {
+                    '3000/tcp': [{HostPort: `${availablePort}`}]
+                }
+            }
         }, {
             params: {
             name: `${containerName}`,
             }
-        });
+        },
+        
+    );
+
+    // console.log(createResponse);
+
+//     "PortBindings": {
+// "22/tcp": [
+// {
+// "HostPort": "11022"
+// }
+// ]
+// },
         const containerId = createResponse.data.Id;
 
         // Start container
         await docker.post(`/containers/${containerId}/start`);
+
+//         const containerInfo = await docker.get(`/containers/${containerId}/json`);
+// console.log(containerInfo.data.HostConfig.PortBindings);
+
+        ContainerTOMaPortMap[containerId] = availablePort;
+        PortToContainerMap[availablePort] = containerId;
 
         res.send(`Container started with ID: ${containerId}`);
     } catch (error) {
@@ -59,7 +104,15 @@ app.post('/stop-container', async (req, res) => {
     }
 });
 
-
+app.get('/get-containers', async (req, res) => {
+    try {
+        const lsContainers = await docker.get('/containers/json');
+        res.status(200).json(lsContainers.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Failed to get containers');
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
